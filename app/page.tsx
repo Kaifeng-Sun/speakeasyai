ffcdf25d66f69fe4b636805b2c89a6dfa90d83ab
Kaifeng-Sun <skf951130@gmail.com>
2024-10-07 15:30:47 +1100

    add divider component

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ import Banner from "@/components/Home/Banner";
 import HowItWorks from "@/components/Home/HowItWorks";
 import Pricing from "@/components/Home/Pricing";
 import Footer from "@/components/Home/Footer";
+import Divider from "@/components/Home/Divider";
 
 export default function Home() {
   return (
     <main className="mx-auto w-full inset-0 h-full bg-[radial-gradient(#e5e7eb_1px), transparent_1px] [background-size:16px_16px]">
       <Banner></Banner>
-      {/* <Divider></Divider> */}
+      <Divider/>
       <HowItWorks></HowItWorks>
       {/* <Divider></Divider> */}
       <Pricing></Pricing>
---
+++ components/Home/Divider.tsx
@@ -0,0 +1,14 @@
+import { Dot } from 'lucide-react'
+import React from 'react'
+
+function Divider() {
+  return (
+    <div className='flex items-center justify-center'>
+        <Dot className='text-purple-400'/>
+        <Dot className='text-purple-400'/>
+        <Dot className='text-purple-400'/>
+    </div>
+  )
+}
+
+export default Divider
